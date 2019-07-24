e7641eab83fac3f789cddcc2211a13968049d99a
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-07-24 19:44:33 -0400

    added icons to tab bar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,41 @@
+import React from 'react';
 import {
   createBottomTabNavigator,
   createAppContainer,
 } from 'react-navigation'
+import Icon from 'react-native-vector-icons/FontAwesome';
 import { DeckSection, SettingsSection, ExploreSection} from './sections/Stacks'
+
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 export default createAppContainer(createBottomTabNavigator(
   {
     "My Decks": DeckSection,
     Explore: ExploreSection,
     Settings: SettingsSection
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case "My Decks":
+            iconName = "newspaper-o"
+            break;
+          case "Explore":
+            iconName = "rocket"
+            break;
+          case "Settings":
+            iconName = "tasks" //"wrench"
+            break;
+        }
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'teal',
+      inactiveTintColor: 'gray',
+    },
   }
 ));
